c548c66dc3bea7df821c860865b290ad03b4bb5e
John Nguyen <nguyenduyjohn180@gmail.com>
2018-11-16 16:16:22 -0800

    minor changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_Auto/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_Auto/TestAuto.java
@@ -3,12 +3,7 @@ package org.firstinspires.ftc.teamcode._Auto;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.ColorSensor;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
@@ -22,59 +17,22 @@ import java.util.Locale;
 @Autonomous(name="TestAuto", group="Autonomous")
 public class TestAuto extends OpMode {
 
-    private ElapsedTime runtime = new ElapsedTime();
-
-    private DcMotor leftfrontDrive = null;
-    private DcMotor rightfrontDrive = null;
-    private DcMotor leftbackDrive = null;
-    private DcMotor rightbackDrive = null;
-
-    boolean bDebug = false;
-
-    private DcMotor armActivator = null;
-    private Servo markerArm;
-
     private BNO055IMU imu;
 
     @Override
     public void init() {
 
-        try {
-            leftfrontDrive = hardwareMap.get(DcMotor.class, "frontLeft");
-            leftfrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
 
-            rightfrontDrive = hardwareMap.get(DcMotor.class, "frontRight");
-            rightfrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            rightfrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
 
-
-            leftbackDrive = hardwareMap.get(DcMotor.class, "backLeft");
-            leftbackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-            rightbackDrive = hardwareMap.get(DcMotor.class, "backRight");
-            rightbackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            rightbackDrive.setDirection(DcMotor.Direction.REVERSE);
-
-            markerArm = hardwareMap.get(Servo.class, "markerArm");
-            markerArm.setPosition(0);
-
-            armActivator = hardwareMap.get(DcMotor.class, "armActivator");
-            armActivator.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-            BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-            parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
-            parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-            parameters.calibrationDataFile = "BNO055IMUCalibration.json";
-            parameters.loggingEnabled = true;
-            parameters.loggingTag = "IMU";
-            parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
-
-            imu = hardwareMap.get(BNO055IMU.class, "imu");
-            imu.initialize(parameters);
-
-        } catch (IllegalArgumentException iax) {
-            bDebug = true;
-        }
     }
 
     @Override
@@ -84,7 +42,6 @@ public class TestAuto extends OpMode {
 
     @Override
     public void loop() {
-        runtime.reset();
 
         final Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
         telemetry.addLine()
@@ -92,6 +49,11 @@ public class TestAuto extends OpMode {
                 .addData("calib", imu.getCalibrationStatus().toString())
                 .addData("heading", Double.parseDouble(formatAngle(angles.angleUnit, angles.firstAngle)) % 360);
         telemetry.update();
+    }
+
+    @Override
+    public void stop() {
+
     }
 
     String formatAngle(AngleUnit angleUnit, double angle) {
